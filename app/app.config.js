409abb2aa21d831ef88359f7738008ef13331974
santi345-67 <gartosanti@gmail.com>
2026-06-09 20:51:19 +0000

    Disable web plugin to eliminate Expo CLI warnings

--- app/app.config.js
+++ app/app.config.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 export default ({ config }) => ({
   ...config,
+  plugins: [],
   extra: {
     SUPABASE_URL: process.env.SUPABASE_URL || '',
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || '',
